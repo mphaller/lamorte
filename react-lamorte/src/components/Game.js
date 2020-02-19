d1dc97add9a6ae7c58cab75857eef32ca581a18c
Matthew Haller <mphaller@gmail.com>
2020-02-19 15:03:43 -0500

    Dealing and Play Area Stub

--- react-lamorte/src/components/Game.js
+++ react-lamorte/src/components/Game.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import PlayArea from './PlayArea';
 import Player from './Player';
 
 import '../styles/Game.css';
 
-const Game = (props) => {
+const Game = props => {
   const renderPlayers = () => props.state.players.map(player => (
       <Player { ...player } debug={props.debug} />
   ));
+  const renderPlayArea = () => (
+    <PlayArea deck={props.deck} />
+  )
   return (
     <div className="Game">
+      {renderPlayArea()}
       {renderPlayers()}
     </div>
   );
---
+++ react-lamorte/src/components/PlayArea.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Card from './Card';
+
+import '../styles/PlayArea.css';
+
+const PlayArea = props => {
+    const { deck } = props;
+    const renderTalon = () => {
+        if (deck && deck.length) {
+            const talon = deck[0];
+            return (
+                <Card { ...talon } />
+            );
+        } return null;
+    }
+    return (
+      <div className="PlayArea">
+          <div className="Talon">
+              {renderTalon()}
+          </div>
+      </div>
+    );
+}
+
+export default PlayArea;
